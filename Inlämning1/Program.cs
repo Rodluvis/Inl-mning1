685b509c6fc097d1cec63e84fbc6b5abffe22df3
Yulia Rokotova <fastlagsgatan@gmail.com>
2020-11-22 10:41:30 +0100

    Have added logic to account for priority of operators */.

--- Inlämning1/Program.cs
+++ Inlämning1/Program.cs
@@ -25,68 +25,98 @@ namespace Inlämning1
                 Console.WriteLine("Enter third term");
                 int num3 = Convert.ToInt32(Console.ReadLine());
                 int result = 0;
-                switch (operand1)
+                if (operand2 == "*" || operand2 == "/")
                 {
-                    case "+":
-                        num1 += num2;
-                        break;
-                    case "-":
-                        num1 -= num2;
-                        break;
-                    case "*":
-                        num1 *= num2;
-                        break;
+                    switch (operand2)
+                    {
+                        case "*":
+                            num2 = num2 * num3;
+                            break;
+                        case "/":
+                            num2 = num2 / num3;
+                            break;
+                    }
+                    switch (operand1)
+                    {
+                        case "+":
+                            result = num1 + num2;
+                            Console.WriteLine($"The result of your input is: {result}");
+                            break;
+                        case "-":
+                            result = num1 - num2;
+                            Console.WriteLine($"The result of your input is: {result}");
+                            break;
+                        case "*":
+                            result = num1 * num2;
+                            Console.WriteLine($"The result of your input is: {result}");
+                            break;
 
-                    case "/":
-                        num1 /= num2;
-                        break;
+                        case "/":
+                            result = num1 / num2;
+                            Console.WriteLine($"The result of your input is: {result}");
+                            break;
+                    }
+
+
+                }
+                else
+                {
+                    switch (operand1)
+                    {
+                        case "+":
+                            num1 += num2;
+                            break;
+                        case "-":
+                            num1 -= num2;
+                            break;
+                        case "*":
+                            num1 *= num2;
+                            break;
+
+                        case "/":
+                            num1 /= num2;
+                            break;
+                    }
+
+
+                    switch (operand2)
+                    {
+                        case "+":
+                            result = num1 + num3;
+                            Console.WriteLine($"The result of your input is: {result}");
+                            break;
+                        case "-":
+                            result = num1 - num3;
+                            Console.WriteLine($"The result of your input is: {result}");
+                            break;
+                       
+                    }
                 }
 
-                switch (operand2)
-                {
-                    case "+":
-                        result = num1 + num3;
 
-                        Console.WriteLine($"The result of your input is: {result}");
-                        break;
-                    case "-":
-                        result = num1 - num3;
-
-                        Console.WriteLine($"The result of your input is: {result}");
-                        break;
-                    case "*":
-                        result = num1 * num3;
-
-                        Console.WriteLine($"The result of your input is: {result}");
-                        break;
-                    case "/":
-                        result = num1 / num3;
-                        Console.WriteLine($"The result of your input is: {result}");
-                        break;
-                }
                 resultList.Add(result);
-                if (result <100)
+                if (result < 100)
                 {
-                   Console.WriteLine("The sum is less then a hundred.");
+                    Console.WriteLine("The sum is less then a hundred.");
                 }
 
                 if (result == 100)
                 {
-                    Console.WriteLine("Cool, now you have a hundred, clap clap");
+                    Console.WriteLine("Cool, now you have a hundred, clap clap!");
                 }
-               if (result >100)
+                if (result > 100)
                 {
-                    Console.WriteLine("More then a hundred");
+                    Console.WriteLine("More then a hundred.");
                 }
                 Console.WriteLine("Another try? " +
                     "\nPress ENTER to continue." +
-                    "\nPress \"x\" to exit and to print out the sum of all previous calculations");
+                    "\nPress \"x\" to exit and to print out the sum of all previous calculations.");
 
                 string exit = Console.ReadLine().ToLower();
                 switch (exit)
                 {
                     case "x":
-                        Console.WriteLine($"Thank you for playing. The sum of all round is: {resultList.Sum()}");
+                        Console.WriteLine($"Thank you for playing. The sum of all rounds of previous calculations is: {resultList.Sum()}");
                         running = false;
                         return;
                 }
